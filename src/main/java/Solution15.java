386e9f4ee3a87bb1f6b6ec205ab8c88dcf5a43a1
chenwei <cw_163mail@163.com>
2024-04-17 21:07:38 +0800

    finish 15

---
+++ src/main/java/Solution15.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1, k = nums.length - 1; j < k; ) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    list.add(List.of(nums[i], nums[j], nums[k]));
+                    j++;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    k--;
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return list;
+    }
+}
